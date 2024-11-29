02d75845273f6d5f4f209848624c91d984db914c
darojatulaliyah31 <154003108+darojatulaliyah31@users.noreply.github.com>
2024-11-29 17:00:22 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+const app = express();
+app.use(bodyParser.json());
+
+// Import routers
+const usersRouter = require("./src/users");
+const favoriteRouter = require("./src/favorite");
+
+// Endpoint utama
+app.get("/", (req, res) => {
+  res.send("Service Recommendation API");
+});
+
+// Gunakan routers
+app.use("/api/users", usersRouter(db));
+app.use("/api/favorites", favoriteRouter(db));
+
+// Jalankan server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
